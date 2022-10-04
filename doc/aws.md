e03c8db54b87f1c3878308f83931b510e15876ae
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2022-10-04 04:40:15 +0200

    creating a udagram-application

---
+++ doc/aws.md
@@ -0,0 +1,24 @@
+# Project Proposal
+
+Hosting a Full-Stack Application
+
+## Project Plan
+
+- creating database
+
+- then creating s3 bucket
+  and uploading build file from frontend
+
+- create ELastic beanstalk
+
+- AWS Elastic Beanstalk is an orchestration service offered by Amazon Web Services for deploying applications which orchestrates various AWS services, including EC2, S3, Simple Notification Service
+
+- then checking environment health and if green ok
+
+then time for pipeline
+
+## by using circleci
+
+- connecting github or repo with circleci
+- then add config.yml file
+  and add environment variable then it doing the lest of work for build and deploy
---
+++ udagram/udagram-api/src/config/config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// ENV variables
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
+
+export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.PORT),
+  databasePort: Number(process.env.DataBase_PORT),
+  host: process.env.POSTGRES_HOST,
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  dialect: "postgres",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
