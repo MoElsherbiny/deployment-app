f052d7fc748f2c8b4f53f8d439d6c6270243150c
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2022-10-05 08:05:50 +0200

    edit config.yml

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://localhost:810
